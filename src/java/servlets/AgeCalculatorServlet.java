a9dde33916159672a99a66503883aea387bbaf06
659159 <659159@elf7it218883.ACDM.DS.SAIT.CA>
2018-09-13 13:31:53 -0600

    Completed Part 1 of lab

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 659159
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String age = request.getParameter("age");
+
+        if (age == null || age.equals("")) {
+            request.setAttribute("message", "You must give your age");
+            getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+            return;
+        }
+        for (int i = 0; i < age.length(); i++) {
+            if (!Character.isDigit(age.charAt(i))) {
+                request.setAttribute("message", "You must enter a number");
+                getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+                return;
+            }
+
+        }
+
+        request.setAttribute("message", "Your age next birthday will be " + age);
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+
+    }
+
+}
---
+++ web/WEB-INF/agecalculator.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : agecalculator
+    Created on : Sep 13, 2018, 12:26:46 PM
+    Author     : 659159
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        <form method="post" action="age">
+               Enter you age: <input type="text" name="age" value="${age}"><br>
+               <input type="submit" value="Age next birthday">
+        </form>
+          <div>
+            ${message}
+        </div>
+        
+    </body>
+</html>
